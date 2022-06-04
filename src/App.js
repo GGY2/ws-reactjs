442a3a6cc76e2f30926ddd455e6600a1685f0c77
GGY2 <888_wrestling@mail.ru>
2022-06-04 12:46:51 +0200

    initilizing react project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Registration_Form from './Registration_Form';
+
+function App() {
+  return (
+    <div className="App">
+      <Registration_Form />
+    </div>
+  );
+}
+
+export default App;
